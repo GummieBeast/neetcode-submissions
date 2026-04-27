fd499a97e6d95fc4910fe552d6655e9c93344f26
GummieBeast <tydagummie@gmail.com>
2026-04-27 14:39:40 -0500

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,13 @@
+variable = 10
+
+print(type(variable))
+variable = 10.8
+
+print(type(variable))
+variable = False
+
+print(type(variable))
+variable = "Hello World!"
+print(type(variable))
+variable = []
+print(type(variable))
