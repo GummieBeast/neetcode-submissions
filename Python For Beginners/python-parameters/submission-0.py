b552616a8d3b86538231ff11d66f265ace3b3e8b
GummieBeast <tydagummie@gmail.com>
2026-06-04 07:25:30 -0500

    Add: python-parameters - submission-0

---
+++ Python For Beginners/python-parameters/submission-0.py
@@ -0,0 +1,14 @@
+def farewell(name):
+    first = "Bob"
+    second = "Charlie"
+    msg1 = "Goodbye, " + first
+    msg2 = "Goodbye, " + second
+    msg = "Goodbye, " + name
+    print(msg1)
+    print(msg2)
+    print(msg)
+
+
+
+# don't modify below this line
+farewell("NeetCode")
